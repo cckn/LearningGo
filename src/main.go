63387729781351c365bad12311b8e0f9c17526d5
pds <pds@enitt.co.kr>
2020-07-20 17:03:52 +0900

    단축변수 :=

--- src/main.go
+++ src/main.go
@@ -3,17 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var quantity int
-	var length, width float64
-	var customerName string
-
-	quantity = 4
-	length, width = 1.2, 2.4
-	customerName = "Damon Cole"
+	quantity := 4
+	length, width := 1.2, 2.4
+	customerName := "Damon Cole"
 
 	fmt.Println(customerName)
 	fmt.Println("has ordered", quantity, "sheets")
-	fmt.Println("each with an area of")
-	fmt.Println(length*width, "square meters")
-
+	fmt.Println(length * width)
 }
