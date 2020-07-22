dc0b48c2e6d6d92d1ce439b50606d1f61d371e00
pds <pds@enitt.co.kr>
2020-07-22 13:39:15 +0900

    simple game(guess)

--- src/main.go
+++ src/main.go
@@ -12,37 +12,39 @@ import (
 )
 
 func main() {
+	var second = time.Now().Unix()
+	rand.Seed(second)
 
-	rand.Seed(time.Now().Unix())
-	target := rand.Intn(100) + 1
+	var target = rand.Intn(100) + 1
 	fmt.Println("I've chosen a random number between 1 and 100")
+	fmt.Println("Can you guess it?")
 
-	fmt.Println("Target is ", target)
+	var reader = bufio.NewReader(os.Stdin)
 
-	for true {
-		reader := bufio.NewReader(os.Stdin)
-
-		fmt.Print("Make Guess: ")
+	var success = false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println(fmt.Sprintf("You have %d guesses left", 10-guesses))
+		fmt.Printf("Make a guess : ")
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
 		input = strings.TrimSpace(input)
-
 		guess, err := strconv.Atoi(input)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if guess == target {
-			fmt.Println("Correct!")
-			return
+		if guess < target {
+			fmt.Println("Your guess was LOW.")
 		} else if guess > target {
-			fmt.Println("less")
-		} else if guess < target {
-			fmt.Println("more")
+			fmt.Println("Your guess was HIGH.")
+		} else {
+			success = true
+			fmt.Println("GJ! You guessed it!")
+			break
 		}
 	}
-
-	fmt.Println(target)
-
+	if !success {
+		fmt.Println(fmt.Sprintf("Sorry, you didn't guess my number. It was: %d", target))
+	}
 }
