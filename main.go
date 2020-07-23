1800cd0397f5498c7d7e7b6315580d1eb3ef3965
cckn <pds@enitt.co.kr>
2020-07-23 16:11:44 +0900

    slice append

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
-import log "github.com/sirupsen/logrus"
-
 // import log "github.com/sirupsen/logrus"
+import log "github.com/sirupsen/logrus"
 
 func main() {
 
 	defer fileClose()
 
-	notes := []string{"do", "re", "mi", "fa", "so", "la", "ti"}
-	log.Println(notes[3], notes[6], notes[0])
+	slice := []string{"a", "b"}
+	log.Println(slice, len(slice))
+	slice = append(slice, "c", "d")
+	log.Println(slice, len(slice))
+
 }
