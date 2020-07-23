e8e18bb2539f79c7f63890423281fbc4aa1c71c0
cckn <pds@enitt.co.kr>
2020-07-23 17:49:05 +0900

    array -> slice

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
+import (
+	"github.com/cckn/datafile"
+	log "github.com/sirupsen/logrus"
+)
+
 // import log "github.com/sirupsen/logrus"
-import log "github.com/sirupsen/logrus"
 
 func main() {
 
 	defer fileClose()
 
-	slice := []string{"a", "b"}
-	log.Println(slice, len(slice))
-	slice = append(slice, "c", "d")
-	log.Println(slice, len(slice))
+	slice, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Error(err)
+	}
 
+	log.Println(slice)
 }
