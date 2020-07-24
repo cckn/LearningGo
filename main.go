fd8c0e264272ec73cd153e48c51038653a43dd7a
cckn <pds@enitt.co.kr>
2020-07-24 17:10:08 +0900

    내부 구조체, 익명 필드

--- main.go
+++ main.go
@@ -1,38 +1,25 @@
 package main
 
 import (
+	"LearningGo/geo"
+
 	log "github.com/sirupsen/logrus"
 )
 
-func printInfo(s *subscriber) {
-	log.Println(s.name)
-	log.Println(s.rate)
-	log.Println(s.active)
-}
-
-func defaultSubscriber(name string) *subscriber {
-	var s subscriber
-	s.name = name
-	s.rate = 5
-	s.active = true
-
-	return &s
-}
-
-func applyDiscount(s *subscriber) {
-	s.rate = 4.99
-}
 func main() {
 
 	// Template
 	defer fileClose()
 	log.Println("8. 구조체")
-	log.Println("구조체 변수")
+	log.Println("연습문제")
 
-	s1 := defaultSubscriber("aman")
-	applyDiscount(s1)
-	printInfo(s1)
+	location := geo.Landmark{
+		Name: "The GooglePlex",
+		Coordinates: geo.Coordinates{
+			Latitude:  37.42,
+			Longitude: -122.08,
+		},
+	}
 
-	s2 := defaultSubscriber("beth")
-	printInfo(s2)
+	log.Println(location)
 }
