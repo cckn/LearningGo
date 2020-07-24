a58b4178d650b99c8618a2c8f47f6fbd6fb9d599
cckn <pds@enitt.co.kr>
2020-07-24 16:28:59 +0900

    struct pointer

--- main.go
+++ main.go
@@ -4,19 +4,35 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+func printInfo(s *subscriber) {
+	log.Println(s.name)
+	log.Println(s.rate)
+	log.Println(s.active)
+}
+
+func defaultSubscriber(name string) *subscriber {
+	var s subscriber
+	s.name = name
+	s.rate = 5
+	s.active = true
+
+	return &s
+}
+
 func applyDiscount(s *subscriber) {
 	s.rate = 4.99
 }
-
 func main() {
 
 	// Template
 	defer fileClose()
 	log.Println("8. 구조체")
-	log.Println("구조체 내용 변경하기")
+	log.Println("구조체 변수")
 
-	var s subscriber
-	applyDiscount(&s)
-	log.Println(s.rate)
+	s1 := defaultSubscriber("aman")
+	applyDiscount(s1)
+	printInfo(s1)
 
+	s2 := defaultSubscriber("beth")
+	printInfo(s2)
 }
