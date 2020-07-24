8390953e6c6e4ba0e020defc0c495c3778f12641
cckn <pds@enitt.co.kr>
2020-07-24 18:13:42 +0900

    리시버 매개변수

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type MyType string
+
+func (receiver MyType) sayHi() {
+
+	log.Println(receiver)
+}
+
 func main() {
 
 	// Template
@@ -11,4 +18,8 @@ func main() {
 	log.Println("8. 나만의 타입 - 사용자 정의 타입")
 	log.Println("메서드")
 
+	value := MyType("a MyType value")
+	value.sayHi()
+	value2 := MyType("a")
+	value2.sayHi()
 }
