fbf215416b53891e2cff44d6cec1e957d0973062
cckn <cckn1985@gmail.com>
2020-07-26 17:31:54 +0900

    임베딩 연습 문제 #2

--- main.go
+++ main.go
@@ -8,20 +8,14 @@ import (
 func main() {
 	defer fileClose()
 	log.Println("Part.10")
-	log.Println("캡슐화")
 	log.Println("연습 문제")
+	log.Println("landmark getter/setter")
 
-	var place geo.Coordinate
-
-	err := place.SetLatitude(30)
+	var lm geo.Landmark
+	err := lm.SetName("LF outlet")
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = place.SetLongitude(-170)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println(place.Latitude(), place.Longitude())
+	log.Println(lm.Name())
 
 }
