72bfb2c5a8df43faeade01c5fdcc23c544520ecb
cckn <cckn1985@gmail.com>
2020-08-03 23:03:07 +0900

    파일 나열하기(재귀)

--- main.go
+++ main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
+	"io/ioutil"
 )
 
-type CoffeePot string
+func readDir(path string, depth int) {
 
-func (c CoffeePot) String() string {
-	return string(c) + " coffee pot"
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		newPath := fmt.Sprintf("%s/%s", path, file.Name())
+
+		if file.IsDir() {
+			log.Println("File : ", newPath)
+			readDir(newPath, depth+1)
+		} else {
+			log.Println("File : ", newPath)
+		}
+	}
 }
 
 func main() {
 	defer fileClose()
-	log.Println("Part.11")
-	log.Println("Stringer interface")
-
-	coffeePot := CoffeePot("LuxBrew")
-	log.Println(coffeePot.String())
-
+	log.Println("파일 나열 하기")
+	readDir(".", 0)
 }
